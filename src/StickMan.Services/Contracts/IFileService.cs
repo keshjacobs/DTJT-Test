697620c7ae4fefb6cd05fd955575b41939d5e01d
Moinul Islam <32401426+moinuliit1414@users.noreply.github.com>
2019-03-18 00:07:57 +0600

    Update IFileService.cs
    Add file copy method

--- src/StickMan.Services/Contracts/IFileService.cs
+++ src/StickMan.Services/Contracts/IFileService.cs
@@ -3,5 +3,7 @@ namespace StickMan.Services.Contracts
 	public interface IFileService
 	{
 		void SaveFile(int userId, string fileName, string base64Content);
+		
+		void CopyFile(string sourcePath, string destPath);
 	}
 }
